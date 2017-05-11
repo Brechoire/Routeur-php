49207dd083141a4ee563884388d9186595098835
MrStranger1 <mr1strange@gmail.com>
2017-05-11 21:30:22 +0200

    routeur final

--- RessourceRouter/Router.php
+++ RessourceRouter/Router.php
@@ -1,14 +1,57 @@
-<?php 
+<?php
+/**
+ * Class qui sert a gérer les urls
+ */
 class Router
 {
+	/**
+	 * controller a appeler
+	 * @var string
+	 */
 	private static $_controller;
+
+	/**
+	 * toutes les routes stocké
+	 * @var array
+	 */
 	private static $_routes 			= [];
+
+	/**
+	 * les correspondance
+	 * @var array
+	 */
 	private static $_matches 			= [];
+
+	/**
+	 * les correspondance des ressources
+	 * @var array
+	 */
 	private static $_ressourcesMatches 	= [];
+
+	/**
+	 * le tableau des paramètres
+	 * @var array
+	 */
 	private static $_paramMatch 		= [];
+
+	/**
+	 * tableau des paramètres
+	 * @var array
+	 */
 	private static $_params     		= [];
+
+	/**
+	 * la route des ressource a stocké
+	 * @var string
+	 */
 	private static $_ressourceRoute;
 	
+/**
+ * methode pour les ressources
+ * @param  string $route   controlleur commun
+ * @param  array  $actions ensemble des routes
+ * @return void          retourne rien
+ */
 	public static function ressources($route, $actions =[])
 	{
 		$route = trim($route, '/');
@@ -17,12 +60,18 @@ class Router
 		$routes = explode('/', $url);
 		preg_match('#[a-z]+#i', $path, $controller);
 		self::$_ressourceRoute = !empty($routes[0]) ? $routes[0] : 'index';
-		self::$_matches = array_slice($routes, 1); // !important : garder
+		self::$_matches = array_slice($routes, 1);
 		foreach ($actions as $action) {
 			self::$_routes[$action] = $controller[0].'@'.$action;
 		}
 	}
 
+/**
+ * trouver la ressources correspondant dans l'url
+ * @param  string $regex  expression chercher
+ * @param  string $acCtrl controlleur
+ * @return bool         retourne true|false
+ */
 	public static function matchRessource($regex, $acCtrl)
 	{
 		if(!preg_match("#$regex#", $acCtrl)){
@@ -32,27 +81,37 @@ class Router
 		}
 	}
 
+/**
+ * appel le controlleur et la méthode
+ * @param  string $controller controlleur a appeler
+ * @return string|method             retourne la méthode
+ */
 	public static function called($controller)
 	{
 		if (is_string($controller)) {
 			require_once 'controllers/BaseController.php';
 			$params = explode('@', $controller);
 	        $controller = $params[0] . "Controller";
-	        if (!file_exists("controllers/".$controller.'.php')) {
+	        if (!file_exists("controllers/".$controller.'.php')) { //si fichier existe pas
 	        	return self::called('base@notFound');
 	        }
 	        require_once "controllers/".$controller.'.php';
 	        $controller = new $controller();
-	        if (!method_exists($controller, $params[1])) {
-	        	echo "methode exite pas dans ce controleur";
-	        	return self::called('base@notFound');//si fichier exite pas
+	        if (!method_exists($controller, $params[1])) { //si la méthode existe pas
+	        	return self::called('base@notFound');
+	        }
+	        if (empty(self::$_matches)) {
+	        	self::$_matches = [null]; //si params existe pas, il passe un tableau null
 	        }
 	        return call_user_func_array([$controller, $params[1]], self::$_matches);
 		}
 		return call_user_func_array($controller, self::$_matches);
 	}
 
-	//pour les méthodes get
+/**
+ * mise en marche du routeur
+ * @return string|method retourne (l'objet a appeler)
+ */
 	public static function run()
 	{
 		if (empty(self::$_routes)) {
@@ -66,6 +125,12 @@ class Router
 		return self::called('base@notFound');
 	}
 
+/**
+ * créer une route simple
+ * @param  string $route          route a inclure dans le tableau des routes
+ * @param  string $controllerCall controller a appeller
+ * @return object                 retourne l'objet en cours
+ */
 	public static function get($route, $controllerCall)
 	{
 		$route = trim($route, '/');
@@ -73,6 +138,12 @@ class Router
 		return new self;
 	}
 
+/**
+ * trouve la correspondance entre les routes et l'url
+ * @param  string $url   url a comparé
+ * @param  string $route route avec laquelle le comparé
+ * @return bool        retourne si trouvé
+ */
 	public static function match($url, $route)
 	{
 		$path = preg_replace_callback('#{[a-z_]+}#i', 'self::paramMatch', $route);
@@ -81,10 +152,15 @@ class Router
 		    return false;
 		}
 		array_shift($matches);
-		self::$_matches = $matches;
+		self::$_matches = $matches;		
 		return true;
 	}
 
+/**
+ * récupère et stocke les paramètres
+ * @param  string $match correspondance
+ * @return string        retourne le pattern
+ */
 	public static function paramMatch($match)
 	{
 		$matcher = str_replace(['{','}'],'',$match[0]);
@@ -94,6 +170,12 @@ class Router
 		return '([^/]+)\/?';
 	}
 
+/**
+ * vérifier la validité des paramètres
+ * @param  string $param nom du parametre a verifier
+ * @param  string $regex regex a vérifier
+ * @return object        retiourne l'objet en cours
+ */
 	public function with($param, $regex)
 	{
 		self::$_params[$param] = str_replace(['{','}'],'',$regex);

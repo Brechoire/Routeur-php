6409c70eddff4d8df19a6cbf0e093975bea2e02f
MrStranger1 <mr1strange@gmail.com>
2017-05-11 21:32:39 +0200

    ajout commentaires

--- RessourceRouter/controllers/BaseController.php
+++ RessourceRouter/controllers/BaseController.php
@@ -1,17 +1,28 @@
 <?php 
 /**
-* 
+* Controlleur de base: mettre toutes les méthodes dont les autres controlleurs doivent hériter
 */
-class BaseController
+class BaseController extends BaseController
 {
+	/**
+	 * page introuvable
+	 * @return void returne le message de la page introuvable
+	 */
 	public function notFound()
 	{
 		echo "<h1>Page introuvable</h1>";
+		header("HTTP/1.0 404 Not Found");
 	}
 
+/**
+ * verifier de nouveau si le param recu dans le controlleur correspond a un format donné
+ * @param  string $regex le regex, l'expression
+ * @param  string|int $name  le paramettre a verifier
+ * @return bool        retourne true|false
+ */
 	public function with($regex, $name)
 	{
-		if (preg_match("#$regex#iu", $name, $matches)) {
+		if (preg_match("#$regex#i", $name, $matches)) {
 			return true;
 		}
 		return false;
